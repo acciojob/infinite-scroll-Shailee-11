abdbc3a0988abc84a697fc7e0a07b12bd83c9d1a
Shailee-11 <128290114+Shailee-11@users.noreply.github.com>
2023-07-06 15:32:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 //your code here!
+let list = document.querySelector('#myList');
+
+// add 10 list items by default
+for (let i = 0; i < 10; i++) {
+  let listItem = document.createElement('li');
+  listItem.textContent = `List item ${i + 1}`;
+  list.appendChild(listItem);
+}
+
+// add 2 more list items when the user reaches the end of the list
+list.addEventListener('scroll', function() {
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+    for (let i = 0; i < 2; i++) {
+      let listItem = document.createElement('li');
+      listItem.textContent = `List item ${list.children.length + 1}`;
+      list.appendChild(listItem);
+    }
+  }
+});
+
 
